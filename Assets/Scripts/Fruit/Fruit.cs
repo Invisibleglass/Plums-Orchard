cf38af7e72bdf969cb893a9751b48a6f1a51dd23
Invisibleglass <122887946+Invisibleglass@users.noreply.github.com>
2024-07-02 18:21:03 -0600

    First gameplay loop

--- Assets/Scripts/Fruit/Fruit.cs
+++ Assets/Scripts/Fruit/Fruit.cs
@@ -5,4 +5,12 @@ using UnityEngine;
 public class Fruit : MonoBehaviour
 {
     public float Points;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("fruitKiller"))
+        {
+            Destroy(gameObject);
+        }
+    }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,13 +4,72 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private bool gameRunning;
+    private GameObject player1;
+    private GameObject player2;
+
+    [HideInInspector] public float Player1Points;
+    [HideInInspector] public float Player2Points;
+
+    [Header("Players")]
+    public PlayerController player1prefab;
+    public PlayerController player2prefab;
+    [Header("Player Spawns")]
+    public GameObject player1Spawn;
+    public GameObject player2Spawn;
+    [Header("Fruits and Snek")]
+    public List<GameObject> treeSpawnables;
+    [Header("TreeSpawnPoints")]
+    public Transform highLeft;
+    public Transform highRight;
+    public Transform lowLeft;
+    public Transform lowLeftR;
+    public Transform lowRight;
+    public Transform lowRightL;
+    [Header("SpawnVaribles")]
+    public float spawnIntervalMin;
+    public float SpawnIntervalMax;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player1 = Instantiate(player1prefab.gameObject, player1Spawn.transform.position, player1Spawn.transform.rotation);
+        player2 = Instantiate(player2prefab.gameObject, player2Spawn.transform.position, player2Spawn.transform.rotation);
+
+        StartCoroutine(SpawnTreeObjects());
     }
 
-    // Update is called once per frame
+    protected IEnumerator SpawnTreeObjects()
+    {
+        gameRunning = true;
+        while (gameRunning)
+        {
+            // Randomly determine how long to wait before changing direction
+            float waitTime = Random.Range(spawnIntervalMin, SpawnIntervalMax);
+            yield return new WaitForSeconds(waitTime);
+
+            GameObject choosenSpawn = treeSpawnables[Random.Range(0, treeSpawnables.Count)];
+            if (Random.Range(0,5) != 4)
+            {
+                Instantiate(choosenSpawn, new Vector3 (Random.Range(highLeft.position.x, highRight.position.x), highLeft.position.y), choosenSpawn.transform.rotation);
+            }
+            else
+            {
+                int randomNum = (Random.Range(0, 2));
+                Debug.Log("Random Spawn lower = " + randomNum);
+                bool leftOrRight = randomNum == 0 ? true : false;
+                if (leftOrRight)
+                {
+                    Instantiate(choosenSpawn, new Vector3(Random.Range(lowLeft.position.x, lowLeftR.position.x), Random.Range(lowLeft.position.y, lowLeftR.position.y)), choosenSpawn.transform.rotation);
+                }
+                else
+                {
+                    Instantiate(choosenSpawn, new Vector3(Random.Range(lowRight.position.x, lowRightL.position.x), Random.Range(lowRight.position.y, lowRightL.position.y)), choosenSpawn.transform.rotation);
+                }
+            }
+        }
+    }
+        // Update is called once per frame
     void Update()
     {
         
